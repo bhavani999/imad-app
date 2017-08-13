65f62cbd81c2440657a1f7d1a8d4ba9ce0ca6b5e
bhavani999 <30969978+bhavani999@users.noreply.github.com>
2017-08-13 14:53:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  
 });
 app.get('/article-two',function(req,res){
-    res.send('Article two requested and will served here');
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  
 });
 app.get('/article-three',function(req,res){
-    res.send('Article three requested and will served here');
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
