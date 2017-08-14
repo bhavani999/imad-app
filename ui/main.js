3a2d59101c4b9c2c0dfda3503b245182c9ec8df5
bhavani999 <30969978+bhavani999@users.noreply.github.com>
2017-08-14 11:30:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,6 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML =' NEW VALUE';
-var img = document.getElementById('web');
- var marginLeft = 0 ;
- function moveRight(){
-     marginLeft = marginLeft + 1;
-     img.style.marginLeft = marginLeft + 'px';
-
- }
- img.onclick = function(){
-     var interval = setInterval(moveRight,50);
- };
+var button = document.getElementById('counter');
+button.onclick= function() {
+   counter = counter + 1;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
+};
