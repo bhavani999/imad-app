6820bec2c5054f30bea4d4d6b585e60c95e533eb
bhavani999 <30969978+bhavani999@users.noreply.github.com>
2017-08-14 10:53:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.innerHTML =' NEW VALUE';
+var img = document.getElementById('web');
+ var marginleft=0;
+ function moveleft (){
+     marginleft = marginleft + 1;
+     img.style.marginleft = marginleft + 'px';
+
+ }
+ img.onclick = function(){
+     var terminal = setInterval(moveright,50);
+ };
